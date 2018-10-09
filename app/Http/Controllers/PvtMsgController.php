30329117bfa4128841ed438e2271bfc51963c0fa
Aaron Oviedo <aaron.t.o@hotmail.com>
2018-10-09 17:34:32 -0500

    Private Message model and controller added (but blank)

---
+++ app/Http/Controllers/PvtMsgController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Pirategram\Http\Controllers;
+
+use Pirategram\PvtMsg;
+use Illuminate\Http\Request;
+
+class PvtMsgController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \Pirategram\PvtMsg  $pvtMsg
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PvtMsg $pvtMsg)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \Pirategram\PvtMsg  $pvtMsg
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PvtMsg $pvtMsg)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Pirategram\PvtMsg  $pvtMsg
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PvtMsg $pvtMsg)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \Pirategram\PvtMsg  $pvtMsg
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PvtMsg $pvtMsg)
+    {
+        //
+    }
+}
---
+++ app/PvtMsg.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Pirategram;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PvtMsg extends Model
+{
+    //
+}
